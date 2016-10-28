9b000332cd2eb0b5e3fa7fb5b4a3d29818d4c822
Phyllis Torres <ptorres@students.mchenry.edu>
2016-10-28 08:44:37 -0500

    Add files via upload

---
+++ time_16_1.py
@@ -0,0 +1,24 @@
+# Phyllis Torres
+# 16.1 Time Assignment
+# Due 11/10/2016
+
+# create a time class
+
+class Time():
+    """Represents the time of day
+    attributes: hour, minute, second
+    """
+
+def print_time(t):
+    print '%.2d:%.2d:%.2d' % (t.hour, t.minute, t.second)
+
+time = Time()
+time.hour = 10
+time.minute = 30
+time.second = 5
+
+print ('The hour field is 10, the minute field is 30, and the second field is 5. \n')
+print ('This input will be reformatted to a conventional format: \n')
+
+print_time(time)
+
